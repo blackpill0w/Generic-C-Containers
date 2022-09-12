5c2b79f5636b809cef41b62788eba240811580bf
blackpill0w <blackpill0w@protonmail.com>
2022-09-12 14:39:53 +0200

    Add vec_pop()

--- include/vector.h
+++ include/vector.h
@@ -1,3 +1,4 @@
+/*! \file */
 #ifndef _GENERIC_VECTOR_H
 #include <stdlib.h>
 #include <string.h>
@@ -12,7 +13,10 @@
    } vec_##NAME_SUFFIX;
 
 
-//! Free allocated resources.
+/*!
+  \def vec_free(vec)
+  Free allocated resources.
+*/
 #define vec_free(vec) free((vec).vec)
 
 //! Get vector's length.
@@ -37,14 +41,31 @@
    }
 
 //! Get an item from the vector using an index, abort if the index is out of bound.
-#define vec_get(vec, i) ({                                                       \
-   if (i > (vec)._capacity) {                                                    \
-      fprintf(stderr, "\nError: accessing out of bound memory, aborting...\n");  \
-      abort();                                                                   \
-   }                                                                             \
-   (vec).v[i];                                                                   \
+#define vec_get(vec, i) ({                                            \
+   if (i > (vec)._capacity) {                                         \
+      fprintf(stderr,                                                 \
+              "\nError: accessing out of bound memory, aborting...\n" \
+      );                                                              \
+      abort();                                                        \
+   }                                                                  \
+   (vec).v[i];                                                        \
 })
 
+//! Remove the last item, and return it.
+#define vec_pop(vec)  ({                                              \
+   if ((vec)._len <= 0) {                                             \
+      fprintf(stderr,                                                 \
+              "\nError: accessing out of bound memory, aborting...\n" \
+      );                                                              \
+      abort();                                                        \
+   }                                                                  \
+   (vec).v[(vec)._len-- - 1];                                         \
+})
+
+/*!
+   Increase the capacity of the vector.
+   If n is less than the vector's capacity, no changes are made.
+*/
 #define vec_reserve(vec, n)                                    \
    if ((vec)._capacity < n) {                                  \
       (vec).v = realloc((vec).v, n * sizeof((vec).v[0]) );     \
