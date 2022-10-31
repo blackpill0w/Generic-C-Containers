0373e3db9317b15c05f6a1bc3d5cf027827f57f3
blackpill0w <blackpill0w@protonmail.com>
2022-10-31 20:13:44 +0100

    Change Some Functions' Names

--- include/arr.h
+++ include/arr.h
@@ -9,14 +9,14 @@
   Abort and write an error to stderr.
   @param str: the error message to display.
 */
-#define abort_with_arr_err(str) ({      \
+#define _arr_abort_with_err(str) ({      \
    fprintf(stderr,                      \
       "\nError: %s, aborting...\n", str \
    ); abort(); 0; })
 
-#define out_of_bound_arr_err() abort_with_arr_err("Accessing out of bound memory")
+#define _arr_out_of_bound_err() _arr_abort_with_err("Accessing out of bound memory")
 
-#define memory_allocation_arr_err() abort_with_arr_err("Memory allocation failed")
+#define _arr_memory_allocation_err() _arr_abort_with_err("Memory allocation failed")
 
 /*!
   Define array for the given type, the produced type is arrNAME_SUFFIX.
@@ -34,7 +34,7 @@
 #define arr_init(arr, size)                        \
    arr._len = size;                                \
    arr.a = calloc(size, sizeof( *((arr).a) ));     \
-   if (arr.a == NULL) memory_allocation_arr_err()
+   if (arr.a == NULL) _arr_memory_allocation_err()
 
 /*!
   Free allocated resources.
@@ -48,6 +48,6 @@
 #define arr_len(arr) (arr)._len
 
 //! Get an item from the array using an index, abort if the index is out of bound.
-#define arr_at(arr, i) (arr).a[ (i) >= (arr)._len ? out_of_bound_arr_err() : (i) ]
+#define arr_at(arr, i) (arr).a[ (i) >= (arr)._len ? _arr_out_of_bound_err() : (i) ]
 
 #endif // _GENERIC_ARRAY_H
