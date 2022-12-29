065b4fab137ec8e856b10f0c14e73919f621885c
blackpill0w <blackpill0w@protonmail.com>
2022-12-29 12:55:17 +0100

    Fixed Bug

--- include/vec.h
+++ include/vec.h
@@ -57,13 +57,13 @@
             (vec).v,                                        \
             ((vec)._capacity *= 2) * sizeof( *((vec).v) )   \
          );                                                 \
-         if ((vec).v == NULL) _vec_memory_allocation_err();   \
+         if ((vec).v == NULL) _vec_memory_allocation_err(); \
       }                                                     \
       (vec).v[(vec)._len++] = item;                         \
    }
 
 //! Remove the last item, and return it.
-#define vec_pop(vec) (vec).v[(vec)._len == 0) ? _vec_pop_empty_vec_err() : --((vec)._len) ]
+#define vec_pop(vec) (vec).v[((vec)._len == 0) ? _vec_pop_empty_vec_err() : --((vec)._len) ]
 
 //! Empty the vector.
 #define vec_clear(vec) ((vec)._len = 0)
@@ -75,8 +75,8 @@
 #define vec_reserve(vec, n)                                    \
    if ((vec)._capacity < n) {                                  \
       (vec).v = realloc((vec).v, n * sizeof((vec).v[0]) );     \
-      if ((vec).v == NULL) _vec_memory_allocation_err();          \
+      if ((vec).v == NULL) _vec_memory_allocation_err();       \
       (vec)._capacity = n;                                     \
-   }                                                           \
+   }
 
 #endif // _GENERIC_VECTOR_H
